863cb7f960123b0be2b274dd72ba55a903818a1f
thisaramanohara <thisaramanohara@gmail.com>
2021-12-28 10:32:19 +0530

    Random Forest parameters loaded

--- main.py
+++ main.py
@@ -39,7 +39,12 @@ def add_parameter_ui(clf_name):
     elif classifier_name == 'SVM':
         C = st.sidebar.slider('C', 0.01, 10.0)
         params['C'] = C
+    else:
+        max_depth = st.sidebar.slider('max-depth', 2, 15)
+        n_estimators = st.sidebar.slider('number of estimators', 1, 100)
 
+        params['max_depth'] = max_depth
+        params['n_estimators'] = n_estimators
 
     return params
 
